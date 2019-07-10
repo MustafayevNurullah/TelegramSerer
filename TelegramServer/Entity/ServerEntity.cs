752de8f5da9916b895d2418d55fe6bc7ba88f9ca
Nurullah <nurlan3295@gmail.com>
2019-07-10 22:58:04 +0400

    Add project files.

---
+++ TelegramServer/Entity/ServerEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramServer.Entity
+{
+   public class ServerEntity
+    {
+        public int Id { get; set; }
+        public string SenderIp { get; set; }
+        public string Message { get; set; }
+        public string SentIp { get; set; }
+    }
+}
