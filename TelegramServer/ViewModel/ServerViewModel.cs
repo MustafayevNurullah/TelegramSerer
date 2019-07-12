73e642cde19315496422e49d5309b1cbb61bbdda
Nurullah <nurlan3295@gmail.com>
2019-07-12 21:38:34 +0400

    07.12.2019

--- TelegramServer/ViewModel/ServerViewModel.cs
+++ TelegramServer/ViewModel/ServerViewModel.cs
@@ -18,113 +18,95 @@ namespace TelegramServer.ViewModel
     {
         static string IpAddress = "10.1.16.27";
         static int TcpPort = 1032;
-        // static string IP = "10.1.16.16";
-        NetworkStream NetworkStream;
-
         public ServerViewModel()
         {
             ServerList = new ObservableCollection<ServerEntity>();
             Task task = Task.Run(() => AcceptClient());
             List = new List<ClientEntity>();
-
         }
         TcpListener server = null;
         int counter = 1;
         NetworkStream stream;
-        void Recur()
-        {
 
-            StartTcpServer();
-        }
         public string GetImagePath(byte[] buffer, int counter)
         {
             ImageConverter ic = new ImageConverter();
             Image img = (Image)ic.ConvertFrom(buffer);
             Bitmap bitmap1 = new Bitmap(img);
             Random random = new Random();
-            counter += random.Next(1,100);
+            counter += random.Next(1, 100);
             bitmap1.Save($@"C:\Users\User\Desktop\image{counter}.png");
             var imagepath = $@"C:\Users\User\Desktop\image{counter}.png";
             return imagepath;
         }
-
-
         void StartTcpServer()
         {
             try
             {
-                while(true)
-                {
-
-               string data = null;
-                int j = 0;
                 while (true)
                 {
-                byte[] bytes = new byte[11000000];
-                        int i;
-                       stream = List[j].Client.GetStream();
+
+                    string data = null;
+                    int j = 0;
+                    while (true)
+                    {
+                          byte[] bytes = new byte[11000000];
+                            int i;
+                            stream = List[j].Client.GetStream();
                             if (stream.DataAvailable)
-                        {
-                            i = stream.Read(bytes, 0, bytes.Length);
-                              data = Encoding.ASCII.GetString(bytes, 0, i);
-                            if(data.Contains("PNG"))
                             {
-                                   
-                                   // GetImagePath(bytes,1);
-                              SentClient(bytes, List[j].Id);
+                                i = stream.Read(bytes, 0, bytes.Length);
+                                data = Encoding.ASCII.GetString(bytes, 0, i);
+                                if (data.Contains("PNG"))
+                                {
+                                    SentClient(bytes, List[j].Id);
                                     ServerEntity serverEntity = new ServerEntity();
-                                    serverEntity.Message =data ;
+                                    serverEntity.Message = data;
                                     var action = new Action(() => { ServerList.Add(serverEntity); });
                                     Task.Run(() => App.Current.Dispatcher.BeginInvoke(action)).Wait();
                                     data = null;
-                                bytes = null;
+                                    bytes = null;
+                                }
+                                else
+                                {
+                                    ServerEntity serverEntity = new ServerEntity();
+                                    string[] arr = data.Split('`');
+                                    serverEntity.Message = arr[0];
+                                    var action = new Action(() => { ServerList.Add(serverEntity); });
+                                    Task.Run(() => App.Current.Dispatcher.BeginInvoke(action)).Wait();
+                                    byte[] msg = Encoding.ASCII.GetBytes(arr[0]);
+                                    SentClient(msg, List[j].Id);
+                                }
                             }
-                            else
-                            {
-                            ServerEntity serverEntity = new ServerEntity();
-                            string[] arr = data.Split('`');
-                            serverEntity.Message = arr[0];
-                            var action = new Action(() => { ServerList.Add(serverEntity); });
-                            Task.Run(() => App.Current.Dispatcher.BeginInvoke(action)).Wait();
-                            byte[] msg = Encoding.ASCII.GetBytes(arr[0]);
-                            SentClient(msg, List[j].Id);
-                            }
-                        }
                         j++;
-                    if(j==List.Count)
-                    {
-                        j = 0;
-                    }
+                        if (j == List.Count)
+                        {
+                            j = 0;
+                        }
 
-                }
-                client.Close();
+                    }
+                    client.Close();
                 }
             }
             catch (SocketException e)
             {
                 MessageBox.Show("SocketException: {0}", e.Message);
             }
-            catch( Exception e)
+            catch (Exception e)
             {
                 MessageBox.Show("Exception" + e.Message);
             }
             finally
             {
-                // Stop listening for new clients.
                 server.Stop();
             }
         }
-        Thread thread ;
         TcpClient client = new TcpClient();
         void AcceptClient()
         {
-            bool a = true;
             IPAddress localAddr = IPAddress.Parse(IpAddress);
-            //    TcpListener server = new TcpListener(port);
             server = new TcpListener(localAddr, TcpPort);
-            // Start listening for client requests.
             server.Start();
-
             while (true)
             {
                 client = server.AcceptTcpClient();
@@ -134,17 +116,15 @@ namespace TelegramServer.ViewModel
                 clients.Id = counter;
                 counter++;
                 List.Add(clients);
-                thread = new Thread(Recur);
+                Thread thread = new Thread(StartTcpServer);
                 thread.Start();
             }
         }
         void SentClient(byte[] data1, int Id)
         {
-            // IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(IpAddress), TcpPort);
-            // client.Connect(endPoint);
             foreach (var item in List)
             {
-                if (item.Id !=Id )
+                if (item.Id != Id)
                 {
                     stream = item.Client.GetStream();
                     stream.Write(data1, 0, data1.Length);
