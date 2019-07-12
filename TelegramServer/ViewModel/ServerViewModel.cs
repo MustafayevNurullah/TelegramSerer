a2188cf838320b71664778653671e04fc79989c7
Nurullah <nurlan3295@gmail.com>
2019-07-12 06:30:20 +0400

    07.12.2019

--- TelegramServer/ViewModel/ServerViewModel.cs
+++ TelegramServer/ViewModel/ServerViewModel.cs
@@ -6,57 +6,72 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using TelegramServer.Entity;
 
 namespace TelegramServer.ViewModel
 {
-   public class ServerViewModel:BaseViewModel
+    public class ServerViewModel : BaseViewModel
     {
         static string IpAddress = "10.1.16.27";
         static int TcpPort = 1032;
-       // static string IP = "10.1.16.16";
+        // static string IP = "10.1.16.16";
         NetworkStream NetworkStream;
 
         public ServerViewModel()
         {
             ServerList = new ObservableCollection<ServerEntity>();
-                Task task = Task.Run(() => AcceptClient());
+            Task task = Task.Run(() => AcceptClient());
             List = new List<ClientEntity>();
 
         }
-            TcpListener server = null;
+        TcpListener server = null;
         int counter = 1;
         NetworkStream stream;
+        void Recur()
+        {
+
+            StartTcpServer();
+        }
         void StartTcpServer()
         {
             try
             {
-          
-                byte[] bytes = new byte[256];
+
                 string data = null;
+                byte[] bytes = new byte[256];
+                int j = 0;
                 while (true)
                 {
-                        int i;
-                    NetworkStream stream = client.GetStream();
-                        while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    int i;
+                    stream = List[j].Client.GetStream();
+
+                    if ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                         {
                             data = Encoding.ASCII.GetString(bytes, 0, i);
                             ServerEntity serverEntity = new ServerEntity();
                             string[] arr = data.Split('`');
                             serverEntity.Message = arr[0];
-                          // serverEntity.SenderIp = arr[1];
+                            // serverEntity.SenderIp = arr[1];
                             var action = new Action(() => { ServerList.Add(serverEntity); });
-                            Task.Run(()=>App.Current.Dispatcher.BeginInvoke(action));
+                            Task.Run(() => App.Current.Dispatcher.BeginInvoke(action)).Wait();
                             byte[] msg = Encoding.ASCII.GetBytes(data);
-                            SentClient(msg);
+                           // SentClient(msg, List[j].Id);
                         }
+                    j++;
+                    if(j==List.Count)
+                    {
+                        j = 0;
                     }
-                    client.Close();
+
+                }
+                client.Close();
             }
             catch (SocketException e)
             {
-                //MessageBox.Show( "SocketException: {0}", e);
+                MessageBox.Show("SocketException: {0}", e.Message);
             }
             finally
             {
@@ -68,42 +83,38 @@ namespace TelegramServer.ViewModel
         void AcceptClient()
         {
             bool a = true;
-                IPAddress localAddr = IPAddress.Parse(IpAddress);
-                //    TcpListener server = new TcpListener(port);
-                server = new TcpListener(localAddr, TcpPort);
-                // Start listening for client requests.
-                server.Start();
+            IPAddress localAddr = IPAddress.Parse(IpAddress);
+            //    TcpListener server = new TcpListener(port);
+            server = new TcpListener(localAddr, TcpPort);
+            // Start listening for client requests.
+            server.Start();
 
             while (true)
             {
                 client = server.AcceptTcpClient();
                 stream = client.GetStream();
-                if(a)
-                {
-            Task task1 = Task.Run(() => StartTcpServer());
-                    a = false;
-                }
                 ClientEntity clients = new ClientEntity();
                 clients.Client = client;
                 clients.Id = counter;
                 counter++;
                 List.Add(clients);
+                Thread thread = new Thread(Recur);
+                thread.Start();
             }
         }
-        void SentClient(byte[] data1)
+        void SentClient(byte[] data1, int Id)
         {
-           // IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(IpAddress), TcpPort);
-           // client.Connect(endPoint);
-            NetworkStream stream = client.GetStream();
+            // IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(IpAddress), TcpPort);
+            // client.Connect(endPoint);
             foreach (var item in List)
             {
-                if (item.Id == 2)
+                if (item.Id != Id)
                 {
                     stream = item.Client.GetStream();
                     stream.Write(data1, 0, data1.Length);
                 }
             }
-            
+
         }
         ObservableCollection<ServerEntity> serverlist;
         public ObservableCollection<ServerEntity> ServerList
